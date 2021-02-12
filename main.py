a132529e19e4d5ee6e2f06588c5bdd7897fb5579
rajesh <rajesh.kr.diwakar@gmail.com>
2021-02-12 18:25:37 +0530

    added data, net flags

--- main.py
+++ main.py
@@ -37,15 +37,18 @@ data_transform = transforms.Compose([
 
 def train(args):
 
-    batch_size = 128
-    hymenoptera_dataset = datasets.ImageFolder(root='dataset/images',
+    batch_size = args.batch_size
+    dataset = datasets.ImageFolder(root=args.data,
                                                transform=data_transform)
-    dataset_loader = torch.utils.data.DataLoader(hymenoptera_dataset,
+    dataset_loader = torch.utils.data.DataLoader(dataset,
                                                  batch_size=128, shuffle=True,
                                                  num_workers=4)
-    dataset_loader_test = torch.utils.data.DataLoader(hymenoptera_dataset,
+    dataset_loader_test = torch.utils.data.DataLoader(dataset,
                                                  batch_size=32, shuffle=True,
                                                  num_workers=1)
+
+    assert len(dataset_loader)>0, 'no image found in data dir'
+
     model_dir = os.path.join('model',args.session)
     output_image_dir = os.path.join(model_dir,'output')
     os.makedirs(model_dir,exist_ok=True)
@@ -54,11 +57,11 @@ def train(args):
 
     writer = SummaryWriter(model_dir)
     vae = DiscreteVAE(
-        image_size = 64,
-        num_layers = 1,          # number of downsamples - ex. 256 / (2 ** 3) = (32 x 32 feature map)
-        num_tokens = 1024,       # number of visual tokens. in the paper, they used 8192, but could be smaller for downsized projects
-        codebook_dim = 256,      # codebook dimension
-        hidden_dim = 64,         # hidden dimension
+        image_size = args.image_size,
+        num_layers = args.num_layers,          # number of downsamples - ex. 256 / (2 ** 3) = (32 x 32 feature map)
+        num_tokens = args.num_tokens,       # number of visual tokens. in the paper, they used 8192, but could be smaller for downsized projects
+        codebook_dim = args.codebook_dim,      # codebook dimension
+        hidden_dim = args.hidden_dim,         # hidden dimension
         num_resnet_blocks = 1,   # number of resnet blocks
         temperature = 0.9,       # gumbel softmax temperature, the lower this is, the harder the discretization
         straight_through = False # straight-through for gumbel softmax. unclear if it is better one way or the other
@@ -69,6 +72,7 @@ def train(args):
     # dataset_size = len(dataset_loader)
     # assert dataset_size>= batch_size, 'dataset (%d) < batch_size(%d) '%(len(dataset_loader), batch_size)
     dataset_size = len(dataset_loader)
+    print("Batch per iteration: ",dataset_size)
     optimizer = optim.Adam(vae.parameters(), lr=0.0001)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10*dataset_size,20*dataset_size], gamma=0.1)
 
@@ -138,9 +142,9 @@ def test(args):
     vae.load_state_dict(torch.load(args.weight))
 
     # load dataset
-    hymenoptera_dataset = datasets.ImageFolder(root='dataset',
+    dataset = datasets.ImageFolder(root='dataset',
                                                transform=data_transform)
-    dataset_loader = torch.utils.data.DataLoader(hymenoptera_dataset,
+    dataset_loader = torch.utils.data.DataLoader(dataset,
                                                  batch_size=4, shuffle=False,
                                                  num_workers=1)
 
@@ -170,6 +174,15 @@ if __name__ =='__main__':
     parser.add_argument('--save_every',default=100,type=int)
     parser.add_argument('--test_every',default=100,type=int)
     parser.add_argument('--resume',default=0,type=int)
+    parser.add_argument('--data',default='data',help='data dir. data/classes/img.jpg')
+    parser.add_argument('--batch_size',default=128,type=int)
+
+    parser.add_argument('--image_size',type=int,default=64)
+    parser.add_argument('--num_layers',type=int,default=1)
+    parser.add_argument('--num_tokens',type=int,default=1024)
+    parser.add_argument('--codebook_dim',type=int,default=256)
+    parser.add_argument('--hidden_dim',type=int,default=64)
+
     parser.add_argument('--test',action='store_true',default=False,help='for testing use --test')  #not really required
     parser.add_argument('--weight',default='weights.pth')
 
